8f1edf9107f978720c4ee7420e794c475e5fbd63
hainnotlazy <hain6621.dev@gmail.com>
2024-03-25 18:12:06 +0700

    feat(frontend): setup proxy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  subTitle?: Observable<Object>;
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.subTitle = this.httpClient.get<Object>("api");
+  }
 }
